e6f742ee625de4896c5bc5fdadd1a583352163e3
jppj8 <jtejada@broncos.uncfsu.edu>
2025-10-31 12:27:19 -0400

    Add sum_list function to calculate list sum
    Implemented the sum_list function to calculate the total of a list of numbers.

--- list_sum.py
+++ list_sum.py
@@ -8,12 +8,18 @@ def sum_list(numbers):
     """
 
     # TODO: set a running total variable
+    total = 0
 
     # TODO: use loop to calculate the sum of all numbers in the list
+    for num in numbers:
+        total += num
 
     # TODO: return the sum
+    return total
 
 
 if __name__ == "__main__":
     nums = [5, 19, 2, 33, -5, 2]
     # TODO: call function sum_list() and send argument 'nums', then print it out for your testing
+    result = sum_list(nums)
+    print(result)
