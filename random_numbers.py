a9aba9565275c028549f18cb154dffe5fdc9120b
jppj8 <jtejada@broncos.uncfsu.edu>
2025-10-31 12:29:15 -0400

    Import random library and generate random numbers
    Imported the random library and implemented random number generation in the generator function.

--- random_numbers.py
+++ random_numbers.py
@@ -1,4 +1,5 @@
 # TODO import the library to randomly generate numbers
+import random
 
 
 def generator(n, starting, ending):
@@ -16,10 +17,13 @@ def generator(n, starting, ending):
     numbers = []
 
     # TODO: use loop to generate n random integers between starting and ending, then append the random number into the list
+    for _ in range(n):
+        rand_num = random.randint(starting, ending)
+        numbers.append(rand_num)
 
     return numbers
 
 
 if __name__ == "__main__":
-    print(generator(5, 1, 10))
-    print(generator(3, -5, 5))
+    print(generator(5, 1, 10))   # 5 numbers between 1 and 10
+    print(generator(3, -5, 5))   # 3 numbers between -5 and 5
