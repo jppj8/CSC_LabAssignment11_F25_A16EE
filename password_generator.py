08b0c68cbdf6b776fb9cf9f6ff5f7d0b446337e2
jppj8 <jtejada@broncos.uncfsu.edu>
2025-10-31 12:28:13 -0400

    Refine password generation function documentation
    Updated docstring to clarify parameters and added comments for future implementation.

--- password_generator.py
+++ password_generator.py
@@ -5,7 +5,7 @@ def generate_password(length, use_digits, use_special):
     """
     Generate a random password with customizable options.
 
-    @param length (int): The length of the password. Default is 12.
+    @param length (int): The length of the password.
     @param use_digits (bool): Whether to include digits (0-9)
     @param use_special (bool): Whether to include special characters
 
@@ -20,21 +20,29 @@ def generate_password(length, use_digits, use_special):
 
     possible_characters = ""
     # TODO: concat lower_letters and upper_letters into 'possible_characters'
+    possible_characters += lower_letters + upper_letters
 
     # TODO: if 'use_digits' is True, concat 'digits' to 'possible_characters'
+    if use_digits:
+        possible_characters += digits
 
     # TODO: if 'use_special' is True, concat 'specials' to 'possible_characters'
+    if use_special:
+        possible_characters += specials
 
     # TODO: build a list from 'possible_characters' so that each character is a list item
+    char_list = list(possible_characters)
 
     password = ""
     # TODO: use loop to generate the random password
+    for _ in range(length):
+        password += random.choice(char_list)
 
     return password
 
 
 if __name__ == "__main__":
-    print(generate_password(12, True, True))
-    print(generate_password(14, True, False))
-    print(generate_password(16, False, True))
-    print(generate_password(18, False, False))
+    print(generate_password(12, True, True))    # length 12, include digits + special chars
+    print(generate_password(14, True, False))   # length 14, include digits, no special chars
+    print(generate_password(16, False, True))   # length 16, include special chars, no digits
+    print(generate_password(18, False, False))  # length 18, letters only
